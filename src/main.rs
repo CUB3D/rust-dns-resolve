e04880a54e645278b5a61215303742fade46d790
CUB3D <callumthom11@gmail.com>
2020-02-18 15:01:48 +0000

    First commit

---
+++ src/main.rs
@@ -0,0 +1,143 @@
+use std::net::UdpSocket;
+use byteorder::{ByteOrder, BigEndian};
+
+#[derive(Debug)]
+struct QueryHeader {
+    identification: u16,
+    flags: u16,
+    question_count: u16,
+    answer_count: u16,
+    authority_records_count: u16,
+    additional_records_count: u16
+}
+
+impl Default for QueryHeader {
+    fn default() -> QueryHeader {
+        QueryHeader {
+            identification: 0,
+            flags: 0,
+            question_count: 0,
+            answer_count: 0,
+            authority_records_count: 0,
+            additional_records_count: 0
+        }
+    }
+}
+
+#[derive(Debug)]
+struct QueryQuestion {
+    name: String,
+    type_: u16,
+    class: u16
+}
+
+impl Default for QueryQuestion {
+    fn default() -> QueryQuestion {
+        QueryQuestion {
+            name: "".to_string(),
+            type_: 0,
+            class: 0
+        }
+    }
+}
+
+#[derive(Debug)]
+struct QueryAnswer {}
+
+impl Default for QueryAnswer {
+    fn default() -> QueryAnswer {
+        QueryAnswer {}
+    }
+}
+
+#[derive(Debug)]
+struct QueryAuthority {}
+
+impl Default for QueryAuthority {
+    fn default() -> QueryAuthority {
+        QueryAuthority {}
+    }
+}
+
+#[derive(Debug)]
+struct Query {
+    header: QueryHeader,
+    questions: Vec<QueryQuestion>,
+    answer: QueryAnswer,
+    authority: QueryAuthority
+}
+
+impl Default for Query {
+    fn default() -> Query {
+        Query {
+            header: QueryHeader::default(),
+            questions: Vec::new(),
+            answer: QueryAnswer::default(),
+            authority: QueryAuthority::default()
+        }
+    }
+}
+
+
+fn main() -> std::io::Result<()> {
+    {
+        println!("Socket create");
+        let mut socket = UdpSocket::bind("0.0.0.0:53").expect("Unable to create socket");
+
+        // Receives a single datagram message on the socket. If `buf` is too small to hold
+        // the message, it will be cut off.
+        let mut buf = [0; 128];
+        println!("recv");
+        let (amt, src) = socket.recv_from(&mut buf).expect("No data");
+        println!("Got data: {:?}", &buf.to_vec());
+//        println!("Test: {}", String::from_utf8(buf.to_vec()).expect("Not valid"));
+//
+        let mut query = Query::default();
+        query.header.identification = BigEndian::read_u16(&buf[0..2]);// (buf[0] << 1 & buf[1]) as u16;
+        query.header.flags = BigEndian::read_u16(&buf[2..4]);
+        query.header.question_count = BigEndian::read_u16(&buf[4..6]);
+        query.header.answer_count = BigEndian::read_u16(&buf[6..8]);
+        query.header.authority_records_count = BigEndian::read_u16(&buf[8..10]);
+        query.header.additional_records_count = BigEndian::read_u16(&buf[10..12]);
+
+        for _question_index in 0..query.header.question_count {
+            let mut question = QueryQuestion::default();
+            // Read the label
+            let mut label_pos = 12;
+            let mut label = "".to_string();
+            loop {
+                let label_len = buf[label_pos];
+                label_pos += 1;
+
+                if label_len == 0 {
+                    break;
+                }
+
+                for _label_char_index in 0..label_len {
+                    label = format!("{}{}", label, buf[label_pos] as char);
+                    label_pos += 1;
+                    println!("label: {}", label);
+                }
+            }
+            question.name = label;
+            question.type_ = BigEndian::read_u16(&buf[label_pos..label_pos+2]);
+            label_pos += 2;
+            question.class = BigEndian::read_u16(&buf[label_pos..label_pos+2]);
+
+            query.questions.push(question);
+//            println!("Test: {}", String::from_utf8(labelBuf.to_vec()).expect("Not valid"));
+        }
+
+        println!("Got query: {:?}", query);
+
+
+        let mut response = Query::default();
+        response.header.answer_count = 1;
+
+
+
+        socket.send_to(&buf, &src)?;
+        
+    } // the socket is closed here
+    Ok(())
+}
