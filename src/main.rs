279faedac4050cce4f28dd2bb722704a39698e6c
CUB3D <callumthom11@gmail.com>
2020-02-18 18:13:37 +0000

    stub resolving done

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@ impl Default for QueryHeader {
 }
 
 impl QueryHeader {
-    fn write(self, buf: &mut Vec<u8>) {
+    fn write(&self, buf: &mut Vec<u8>) {
         buf.write_u16::<BigEndian>(self.identification).unwrap();
         buf.write_u16::<BigEndian>(self.flags).unwrap();
         buf.write_u16::<BigEndian>(self.question_count).unwrap();
@@ -81,8 +81,8 @@ impl Default for QueryQuestion {
 }
 
 impl QueryQuestion {
-    fn write(self, buf: &mut Vec<u8>) {
-        for name_token in self.name {
+    fn write(&self, buf: &mut Vec<u8>) {
+        for name_token in &self.name {
             name_token.write(buf);
         }
 
@@ -117,7 +117,7 @@ impl Default for QueryAnswer {
 }
 
 impl QueryAnswer {
-    fn write(self, buf: &mut Vec<u8>) {
+    fn write(&self, buf: &mut Vec<u8>) {
         for name_token in &self.name {
             name_token.write(buf);
         }
@@ -146,7 +146,7 @@ impl Default for QueryAuthority {
 }
 
 impl QueryAuthority {
-    fn write(self, buf: &mut Vec<u8>) {}
+    fn write(&self, buf: &mut Vec<u8>) {}
 }
 
 #[derive(Debug)]
@@ -182,25 +182,25 @@ impl Query {
         }
     }
 
-    fn write(self, buf: &mut Vec<u8>) {
+    fn write(&self, buf: &mut Vec<u8>) {
         self.header.write(buf);
         
-        for question in self.questions {
+        for question in &self.questions {
             question.write(buf);
         }
 
-        for answer in self.answers {
+        for answer in &self.answers {
             answer.write(buf);
         }
 
-        for authority in self.authorities {
+        for authority in &self.authorities {
             authority.write(buf);
         }
     }
 }
 
 fn socket_read_query(socket: &UdpSocket) -> Query {
-    let mut buf = [0; 128];
+    let mut buf = [0; 512];
     println!("recv");
     let (amt, src) = socket.recv_from(&mut buf).expect("No data");
     println!("Got data: {:?}", &buf.to_vec());
@@ -214,10 +214,10 @@ fn socket_read_query(socket: &UdpSocket) -> Query {
     query.header.authority_records_count = BigEndian::read_u16(&buf[8..10]);
     query.header.additional_records_count = BigEndian::read_u16(&buf[10..12]);
 
+    let mut label_pos = 12;
     for _question_index in 0..query.header.question_count {
         let mut question = QueryQuestion::default();
         // Read the label
-        let mut label_pos = 12;
         loop {
             let mut label = "".to_string();
             let label_len = buf[label_pos];
@@ -236,9 +236,50 @@ fn socket_read_query(socket: &UdpSocket) -> Query {
         question.type_ = BigEndian::read_u16(&buf[label_pos..label_pos+2]);
         label_pos += 2;
         question.class = BigEndian::read_u16(&buf[label_pos..label_pos+2]);
+        label_pos += 2;
 
         query.questions.push(question);
 //            println!("Test: {}", String::from_utf8(labelBuf.to_vec()).expect("Not valid"));
+    }
+
+    for _answer_index in 0..query.header.answer_count {
+        let mut answer = QueryAnswer::default();
+        loop {
+            let mut label = "".to_string();
+            let label_len = buf[label_pos];
+            println!("Got answer label: {}", label_len);
+            label_pos += 1;
+            break; //TODO: why does no name get sent with a length of 192
+
+            if label_len == 0 {
+                break;
+            }
+
+            for _label_char_index in 0..label_len {
+                label = format!("{}{}", label, buf[label_pos] as char);
+                label_pos += 1;
+            }
+            answer.name.push(QueryToken::new(label.as_str()));
+            
+        }
+        label_pos+=1;//TODO: why is this needed:
+        answer.type_ = BigEndian::read_u16(&buf[label_pos..label_pos+2]);
+        label_pos+=2;
+        answer.class = BigEndian::read_u16(&buf[label_pos..label_pos+2]);
+        label_pos+=2;
+        answer.ttl = BigEndian::read_u32(&buf[label_pos..label_pos+4]);
+        label_pos+=4;
+        answer.rd_length = BigEndian::read_u16(&buf[label_pos..label_pos+2]);
+        label_pos+=2;
+
+        println!("Answer: {:?}", answer);
+        println!("RD_len: {}", answer.rd_length);
+
+        for _rd_index in 0..answer.rd_length {
+            answer.r_data.push(buf[label_pos]);
+            label_pos+=1;
+        }
+        query.answers.push(answer);
     }
 
     println!("Got query: {:?}", query);
@@ -273,9 +314,21 @@ fn main() -> std::io::Result<()> {
         response.answers[0].r_data = vec![101, 202, 123, 111];
 
 
-        let mut resp_bytes: Vec<u8> = Vec::new();
+/*        let mut resp_bytes: Vec<u8> = Vec::new();
         response.write(&mut resp_bytes);
-        socket.send_to(&resp_bytes[..], &query.requester)?;
+        socket.send_to(&resp_bytes[..], &query.requester)?;*/
+
+        // Forward to local dns
+        let mut req_bytes: Vec<u8> = Vec::new();
+        query.write(&mut req_bytes);
+        socket.send_to(&req_bytes, SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 0, 20)), 53)).unwrap();
+
+        // get response from local dns
+        let resp_local = socket_read_query(&socket);
+        let mut resp_bytes: Vec<u8> = Vec::new();
+        resp_local.write(&mut resp_bytes);
+        socket.send_to(&resp_bytes, query.requester).unwrap();
+
         
     } // the socket is closed here
     Ok(())
